5f8b2f979fec43a514d06d685ac4da118ce62e1c
nitudeka <iamnitudeka@gmail.com>
2023-07-16 15:02:37 +0530

    added handler logic

--- index.js
+++ index.js
@@ -22,10 +22,42 @@ const server = http.createServer((req, res) => {
   req.on("end", () => {
     buffer += decoder.end();
 
-    res.end("hello world\n");
+    const choosenHandler = router[trimmedPath] || handlers.notFound;
+
+    const data = {
+      trimmedPath,
+      queryStringObj,
+      method,
+      headers,
+      payload: buffer,
+    };
+
+    choosenHandler(data, (statusCode, payload) => {
+      statusCode = statusCode || 200;
+      payload = payload || {};
+
+      const payloadString = JSON.stringify(payload);
+
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
   });
 });
 
 server.listen(PORT, () => {
   console.log("The server is listening on port", PORT);
 });
+
+const handlers = {};
+
+handlers.sample = (data, cb) => {
+  cb(406, { name: "sample handler" });
+};
+
+handlers.notFound = (data, cb) => {
+  cb(404);
+};
+
+const router = {
+  sample: handlers.sample,
+};
