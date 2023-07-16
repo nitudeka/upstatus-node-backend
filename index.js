d49eddeba6dd5c699ce3f647d8469233926f18f1
nitudeka <iamnitudeka@gmail.com>
2023-07-16 14:41:45 +0530

    added path and method

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const http = require("http");
+const url = require("url");
 
 const PORT = 4000;
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$g/, "");
+  const method = req.method.toLowerCase();
+
   res.end("hello world\n");
 });
 
